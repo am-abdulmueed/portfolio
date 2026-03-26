dd749e75e67a1080e1b55b11332bac1b8def326a
betapix <177719587+betapix@users.noreply.github.com>
2026-03-26 17:37:51 +0500

    Auto update

--- src/app/blogs/[...slug]/page.jsx
+++ src/app/blogs/[...slug]/page.jsx
@@ -1,3 +1,5 @@
+'use server'
+
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,27 +11,6 @@ import LanguageToggle from '@/components/custom/LanguageToggle';
 import AuthorInfo from '@/components/custom/AuthorInfo';
 
 const DEFAULT_IMAGE = '/images/blog/placeholder.png'; // Placeholder image
-
-export async function generateStaticParams() {
-  try {
-    const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID);
-    const paths = [];
-    const languages = ['en', 'vi']; // Support English and Vietnamese based on your code
-    
-    response.documents.forEach((doc) => {
-      languages.forEach((lang) => {
-        paths.push({
-          slug: [lang, doc.slug]
-        });
-      });
-    });
-    
-    return paths;
-  } catch (error) {
-    console.error('Error generating static params:', error);
-    return [];
-  }
-}
 
 export async function generateMetadata({ params }) {
   const blog = await getBlogPost(params.slug[1]);
